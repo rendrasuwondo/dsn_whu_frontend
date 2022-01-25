5506256c3b63ed39ce0011e934c084358cee2c81
rendrasuwondo <32509861+rendrasuwondo@users.noreply.github.com>
2022-01-25 09:50:18 +0700

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -128,7 +128,7 @@ export default {
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
-    baseURL: 'http://127.0.0.1:8000',
+    baseURL: 'http://103.11.74.84:8082',
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
