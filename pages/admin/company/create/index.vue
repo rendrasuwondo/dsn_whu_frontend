1f5bf650fe9a9a8ee38d411340f8c3614b66e50e
rendrasuwondo <rendrasuwondo@gmail.com>
2022-03-01 15:48:59 +0700

    COmpany edit mar

--- pages/admin/company/create/index.vue
+++ pages/admin/company/create/index.vue
@@ -323,7 +323,7 @@ export default {
     // },
     back() {
       this.$router.push({
-        name: 'admin-activity_plan',
+        name: 'admin-company',
         params: { id: this.$route.params.id, r: 1 },
       })
     },
