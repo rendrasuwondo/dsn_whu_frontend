e953d64f739009628005507031036e599e8eb143
rendrasuwondo <rendrasuwondo@gmail.com>
2022-03-18 21:37:25 +0700

    Site

--- components/admin/sidebar.vue
+++ components/admin/sidebar.vue
@@ -5,7 +5,7 @@
                with font-awesome or any other icon font library -->
           <div v-html="menu"></div>   
           
-          <li class="nav-item has-treeview">
+          <!-- <li class="nav-item has-treeview">
             <a href="#" class="nav-link">
               <i class="nav-icon fas fa-tachometer-alt"></i>
               <p>
@@ -63,7 +63,7 @@
               </li>
             </ul>
           </li>
-        
+         -->
          
         
          
