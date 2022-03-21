0a5e6d8fc6746220dc67cb14acae6c999f25963b
Aida Mahmudah <aida.mahmudah171@gmail.com>
2022-03-21 15:34:03 +0700

    report

--- pages/admin/activity_plan/index.vue
+++ pages/admin/activity_plan/index.vue
@@ -235,7 +235,7 @@ export default {
       ],
 
       activitied_at_start: '',
-      activitied_at_end: '', 
+      activitied_at_end: '',
       show_filter: false,
       //state search
       search: '',
@@ -302,15 +302,11 @@ export default {
     },
 
     filterData() {
-<<<<<<< HEAD
-      show_filter = true
-=======
-      if (this.show_filter==false) {
-         this.show_filter=true 
+      if (this.show_filter == false) {
+        this.show_filter = true
       } else {
-      this.show_filter=false
+        this.show_filter = false
       }
->>>>>>> 1defe3a64ab1740a21fe4180874d5f2219aa292b
     },
 
     //deletePost method
