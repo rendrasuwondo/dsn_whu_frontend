cf777ec8fda5bae1b1f8b93c738594a24d967fd9
Aida Mahmudah <aida.mahmudah171@gmail.com>
2022-04-06 12:02:37 +0700

    exportData

--- pages/system/menu/index.vue
+++ pages/system/menu/index.vue
@@ -205,7 +205,27 @@ export default {
       })
     },
 
-    exportData() {},
+    exportData() {
+      const headers = {
+        'Content-Type': 'application/json',
+      }
+
+      this.$axios({
+        url: `/api/admin/menu/export`,
+        method: 'GET',
+        responseType: 'blob',
+        headers: headers, // important
+      }).then((response) => {
+        this.isLoading = false
+        const url = window.URL.createObjectURL(new Blob([response.data]))
+        const link = document.createElement('a')
+        link.href = url
+        var fileName = 'Menu.xlsx'
+        link.setAttribute('download', fileName) //or any other extension
+        document.body.appendChild(link)
+        link.click()
+      })
+    },
 
     //deletePost method
     deletePost(id) {
