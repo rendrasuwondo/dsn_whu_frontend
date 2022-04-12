235c3307aab2ef941f6fb904dab7bec6405d402b
Aida Mahmudah <aida.mahmudah171@gmail.com>
2022-04-12 11:20:10 +0700

    attendance

--- pages/admin/attendance_sap/index.vue
+++ pages/admin/attendance_sap/index.vue
@@ -13,69 +13,9 @@
           <div class="card-tools"></div>
         </div>
         <div class="card-body">
-          <b-card
-            border-variant="primary"
-            header="Filter"
-            header-bg-variant="info"
-            header-text-variant="white"
-            v-show="show_filter"
-          >
-            <b-card-text>
-              <div>
-                <b-row>
-                  <b-col cols="1">Tanggal</b-col>
-                  <b-col>
-                    <b-input-group>
-                      <b-form-datepicker
-                        v-model="activitied_at_start"
-                        :date-format-options="{
-                          year: 'numeric',
-                          month: 'short',
-                          day: '2-digit',
-                          weekday: 'short',
-                        }"
-                        size="sm"
-                      ></b-form-datepicker>
-                      <template #append>
-                        <b-btn size="sm" @click="activitied_at_start = ''"
-                          ><i class="fa fa-trash"></i
-                        ></b-btn>
-                        &nbsp; s.d
-                      </template>
-                    </b-input-group>
-                  </b-col>
-                  <b-col>
-                    <b-input-group>
-                      <b-datepicker
-                        v-model="activitied_at_end"
-                        reset-button
-                        size="sm"
-                        :date-format-options="{
-                          year: 'numeric',
-                          month: 'short',
-                          day: '2-digit',
-                          weekday: 'short',
-                        }"
-                      ></b-datepicker>
-                      <template #append>
-                        <b-btn size="sm" @click="activitied_at_end = ''"
-                          ><i class="fa fa-trash"></i
-                        ></b-btn>
-                      </template>
-                    </b-input-group>
-                  </b-col>
-                  <b-col></b-col>
-                </b-row>
-              </div>
-            </b-card-text>
-          </b-card>
-
           <div class="form-group">
             <div class="input-group mb-3">
               <div class="input-group-prepend">
-                <button title="Filter" @click="filterData" class="btn btn-info">
-                  <i class="fa fa-filter"></i>
-                </button>
                 <button
                   title="Download Absensi"
                   class="btn btn-info"
@@ -162,11 +102,13 @@ export default {
           key: 'name_employee',
           tdClass: 'align-middle text-left text-nowrap nameOfTheClass',
         },
+
         {
           label: 'Kode Absensi',
           key: 'attendance_type_name',
           tdClass: 'align-middle text-left text-nowrap nameOfTheClass',
         },
+
         {
           label: 'Afdeling',
           key: 'afdeling_code',
@@ -193,41 +135,22 @@ export default {
           tdClass: 'align-middle text-left text-nowrap nameOfTheClass',
         },
       ],
-      activitied_at_start: '',
-      activitied_at_end: '',
-      show_filter: false,
-      //state search
-      search: '',
     }
   },
-  watchQuery: ['q', 'activitied_at_start', 'activitied_at_end'],
+  watchQuery: ['q'],
 
   async asyncData({ $axios, query }) {
     //search
     let search = query.q ? query.q : ''
 
-    //activitied_at_start
-    let activitied_at_start = query.activitied_at_start
-      ? query.activitied_at_start
-      : ''
-
-    //activitied_at_start
-    let activitied_at_end = query.activitied_at_end
-      ? query.activitied_at_end
-      : ''
-
     //fetching posts
-    const posts = await $axios.$get(
-      `/api/admin/attendance_sap?q=${search}&activitied_at_start=${activitied_at_start}&activitied_at_end=${activitied_at_end}`
-    )
+    const posts = await $axios.$get(`/api/admin/attendance_sap?q=${search}`)
     console.log('aida')
     console.log(`/api/admin/attendance_sap?q=${search}`)
     console.log(posts)
     return {
       posts: posts.data,
       search: search,
-      search: search,
-      rowcount: posts.data.total,
     }
   },
 
@@ -239,14 +162,6 @@ export default {
           q: this.search,
         },
       })
-    },
-
-    filterData() {
-      if (this.show_filter == false) {
-        this.show_filter = true
-      } else {
-        this.show_filter = false
-      }
     },
 
     exportData() {
