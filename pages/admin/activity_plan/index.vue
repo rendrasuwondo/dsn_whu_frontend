534de9c330dd86930eb11c890ab3b9caa4406faa
Aida Mahmudah <aida.mahmudah171@gmail.com>
2022-04-13 13:19:04 +0700

    rkh

--- pages/admin/activity_plan/index.vue
+++ pages/admin/activity_plan/index.vue
@@ -18,10 +18,9 @@
             header="Filter"
             header-bg-variant="info"
             header-text-variant="white"
-            v-show="show_filter"
           >
             <b-card-text>
-              <div>
+              <b-container class="bv-example-row mb-3">
                 <b-row>
                   <b-col cols="1">Tanggal</b-col>
                   <b-col>
@@ -66,7 +65,7 @@
                   </b-col>
                   <b-col></b-col>
                 </b-row>
-              </div>
+              </b-container>
             </b-card-text>
           </b-card>
 
@@ -81,13 +80,6 @@
                     title="Tambah"
                     ><i class="fa fa-plus-circle"></i>
                   </nuxt-link>
-                  <button
-                    title="Filter"
-                    @click="filterData"
-                    class="btn btn-info"
-                  >
-                    <i class="fa fa-filter"></i>
-                  </button>
                   <button
                     title="Export To Excel"
                     class="btn btn-info"
@@ -161,22 +153,22 @@
             </template>
           </b-table>
 
-          <b-row>
-            <b-col>
-              <!-- pagination -->
-              <b-pagination
-                v-model="pagination.current_page"
-                :total-rows="pagination.total"
-                :per-page="pagination.per_page"
-                @change="changePage"
-                align="fill"
-                class="mt-3"
-              ></b-pagination>
-            </b-col>
+          <!-- <b-row>
+            <b-col> -->
+          <!-- pagination -->
+          <b-pagination
+            v-model="pagination.current_page"
+            :total-rows="pagination.total"
+            :per-page="pagination.per_page"
+            @change="changePage"
+            align="fill"
+            class="mt-3"
+          ></b-pagination>
+          <!-- </b-col>
             <b-col class="text-right" align-self="center"
               >{{ rowcount }} data</b-col
             >
-          </b-row>
+          </b-row> -->
         </div>
       </div>
     </section>
@@ -252,38 +244,41 @@ export default {
           tdClass: 'align-middle text-left text-nowrap',
         },
       ],
-
-      activitied_at_start: '',
-      activitied_at_end: '',
-      show_filter: false,
-      //state search
-      search: '',
     }
   },
 
   //watch query URL
-  watchQuery: ['q', 'page', 'activitied_at_start', 'activitied_at_end'],
+  watchQuery: ['q', 'page', 'activitied_at_prepend', 'activitied_at_append'],
 
   async asyncData({ $axios, query }) {
+    function currentDate() {
+      const current = new Date()
+      current.setDate(current.getDate())
+      const date = `${current.getFullYear()}-${
+        current.getMonth() + 1
+      }-${current.getDate()}`
+      return date
+    }
+
     //page
     let page = query.page ? parseInt(query.page) : ''
 
     //search
     let search = query.q ? query.q : ''
 
-    //activitied_at_start
-    let activitied_at_start = query.activitied_at_start
-      ? query.activitied_at_start
-      : ''
+    //activitied_at_prepend
+    let activitied_at_start = query.activitied_at_prepend
+      ? query.activitied_at_prepend
+      : currentDate()
 
-    //activitied_at_start
-    let activitied_at_end = query.activitied_at_end
-      ? query.activitied_at_end
-      : ''
+    //activitied_at_append
+    let activitied_at_end = query.activitied_at_append
+      ? query.activitied_at_append
+      : currentDate()
 
     //fetching posts
     const posts = await $axios.$get(
-      `/api/admin/activity_plan?q=${search}&page=${page}&activitied_at_start=${activitied_at_start}&activitied_at_end=${activitied_at_end}`
+      `/api/admin/activity_plan?q=${search}&page=${page}&activitied_at_prepend=${activitied_at_start}&activitied_at_append=${activitied_at_end}`
     )
 
     console.log(posts.data)
@@ -294,6 +289,8 @@ export default {
       pagination: posts.data,
       search: search,
       rowcount: posts.data.total,
+      activitied_at_start: activitied_at_start,
+      activitied_at_end: activitied_at_end,
     }
   },
   mounted() {
@@ -316,8 +313,12 @@ export default {
         query: {
           q: this.$route.query.q,
           page: page,
-          activitied_at_start: this.$route.query.activitied_at_start,
-          activitied_at_end: this.$route.query.activitied_at_end,
+          activitied_at_prepend: this.$route.query.activitied_at_prepend
+            ? this.$route.query.activitied_at_prepend
+            : this.activitied_at_start,
+          activitied_at_append: this.$route.query.activitied_at_append
+            ? this.$route.query.activitied_at_append
+            : this.activitied_at_end,
         },
       })
     },
@@ -328,18 +329,10 @@ export default {
         path: this.$route.path,
         query: {
           q: this.search,
-          activitied_at_start: this.activitied_at_start,
-          activitied_at_end: this.activitied_at_end,
+          activitied_at_prepend: this.activitied_at_start,
+          activitied_at_append: this.activitied_at_end,
         },
       })
-    },
-
-    filterData() {
-      if (this.show_filter == false) {
-        this.show_filter = true
-      } else {
-        this.show_filter = false
-      }
     },
 
     exportData() {
@@ -348,7 +341,7 @@ export default {
       }
 
       this.$axios({
-        url: `/api/admin/activity_plan/export?q=${this.search}&activitied_at_start=${this.activitied_at_start}&activitied_at_end=${this.activitied_at_end}`,
+        url: `/api/admin/activity_plan/export?activitied_at_prepend=${this.activitied_at_start}&activitied_at_append=${this.activitied_at_end}`,
         method: 'GET',
         responseType: 'blob',
         headers: headers, // important
