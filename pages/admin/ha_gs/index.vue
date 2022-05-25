c034432b6804ac957b33f907cfc84783ea02edec
Aida171 <aida.mahmudah171@gmail.com>
2022-05-25 11:04:06 +0700

    export-ha_gs

--- pages/admin/ha_gs/index.vue
+++ pages/admin/ha_gs/index.vue
@@ -343,7 +343,7 @@ export default {
         const url = window.URL.createObjectURL(new Blob([response.data]))
         const link = document.createElement('a')
         link.href = url
-        var fileName = 'HA JS.xlsx'
+        var fileName = 'HA GS.xlsx'
         link.setAttribute('download', fileName) //or any other extension
         document.body.appendChild(link)
         link.click()
