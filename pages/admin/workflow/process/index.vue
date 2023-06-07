865979a019771116d2f7bd47492c5de91027c97f
Nicho Ferdiansyah <nicho.ferdia@gmail.com>
2023-06-07 09:01:38 +0700

    add workflow process page

---
+++ pages/admin/workflow/process/index.vue
@@ -0,0 +1,291 @@
+<template>
+  <div class="content-wrapper mb-5">
+    <section class="content-header">
+      <div class="container-fluid"></div>
+    </section>
+
+    <section class="content">
+      <div class="card card-outline card-info">
+        <div class="card-header">
+          <h3 class="card-title">
+            <i class="nav-icon fas fa-book-open"></i> Workflow
+          </h3>
+          <div class="card-tools"></div>
+        </div>
+        <div class="card-body">
+          <div class="form-group">
+            <div class="input-group mb-3">
+              <div class="input-group-prepend">
+                <nuxt-link
+                  :to="{ name: 'admin-workflow-process-create' }"
+                  class="btn btn-info btn-sm"
+                  style="padding-top: 8px"
+                  title="Tambah"
+                  ><i class="fa fa-plus-circle"></i>
+                </nuxt-link>
+                <button
+                  title="Export To Excel"
+                  class="btn btn-info"
+                  @click="exportData"
+                >
+                  <i class="fa fa-file-excel"></i>
+                </button>
+              </div>
+              <input
+                type="text"
+                class="form-control"
+                v-model="search"
+                @keypress.enter="searchData"
+                placeholder=""
+              />
+              <div class="input-group-append">
+                <button @click="searchData" class="btn btn-info">
+                  <i class="fa fa-search"></i>
+                  CARI
+                </button>
+              </div>
+            </div>
+          </div>
+          <!-- table -->
+          <b-table
+            small
+            responsive
+            striped
+            bordered
+            hover
+            :items="posts"
+            :fields="fields"
+            show-empty
+          >
+            <template v-slot:cell(actions)="row">
+              <b-button
+                :to="{
+                  name: 'admin-workflow-process-edit-id',
+                  params: { id: row.item.id },
+                }"
+                variant="link"
+                size="sm"
+                title="Edit"
+              >
+                <i class="fa fa-pencil-alt"></i>
+              </b-button>
+              <b-button
+                variant="link"
+                size="sm"
+                @click="deleteRole(row.item.id)"
+                title="Hapus"
+                ><i class="fa fa-trash"></i
+              ></b-button>
+            </template>
+            <template v-slot:cell(users)="row">
+              <b-button
+                :to="{
+                  name: 'system-user_has_role-id',
+                  params: { id: row.item.id },
+                }"
+                variant="link"
+                size=""
+                title="Users"
+              >
+                <i class="fa fa-file-alt"></i>
+              </b-button>
+            </template>
+          </b-table>
+          <!-- pagination -->
+          <b-row>
+            <b-col
+              ><b-pagination
+                v-model="pagination.current_page"
+                :total-rows="pagination.total"
+                :per-page="pagination.per_page"
+                @change="changePage"
+                align="left"
+                class="mt-1"
+              ></b-pagination
+            ></b-col>
+            <b-col class="text-right" align-self="center"
+              >{{ rowcount }} data</b-col
+            >
+          </b-row>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'admin',
+
+  head() {
+    return {
+      title: 'Workflow Process',
+    }
+  },
+  data() {
+    return {
+      fields: [
+        {
+          label: 'Actions',
+          key: 'actions',
+          tdClass: 'align-middle text-left text-nowrap nameOfTheClass',
+        },
+        {
+          label: 'Process Name',
+          key: 'proc_name',
+          tdClass: 'align-middle text-center',
+        },
+        {
+          label: 'Display Name',
+          key: 'display_name',
+          tdClass: 'align-middle text-left text-nowrap nameOfTheClass',
+        },
+        {
+          label: 'Taskbox File',
+          key: 'taskbox_file',
+          tdClass: 'align-middle text-left text-nowrap nameOfTheClass',
+        },
+        {
+          label: 'Submit Before',
+          key: 'submit_before',
+          tdClass: 'align-middle text-left text-nowrap nameOfTheClass',
+        },
+        {
+          label: 'Submit After',
+          key: 'submit_after',
+          tdClass: 'align-middle text-left text-nowrap nameOfTheClass',
+        },
+        {
+          label: 'Application Id',
+          key: 'application_id',
+          tdClass: 'align-middle text-left text-nowrap nameOfTheClass',
+        },
+        {
+          label: 'Is Active',
+          key: 'is_active',
+          tdClass: 'align-middle text-left text-nowrap nameOfTheClass',
+        },
+        {
+          label: 'Description',
+          key: 'description',
+          tdClass: 'align-middle text-left text-nowrap nameOfTheClass',
+        },
+        {
+          label: 'Process Code',
+          key: 'proc_code',
+          tdClass: 'align-middle text-left text-nowrap nameOfTheClass',
+        },
+      ],
+      sweet_alert: {
+        title: '',
+        icon: '',
+      },
+    }
+  },
+  watchQuery: ['q', 'page'],
+
+  async asyncData({ $axios, query }) {
+    //page
+    let page = query.page ? parseInt(query.page) : ''
+
+    //search
+    let search = query.q ? query.q : ''
+
+    //fetching posts
+    const posts = await $axios.$get(`/api/admin/workflow/process?q=${search}&page=${page}`)
+
+    return {
+      posts: posts.data.data,
+      pagination: posts.data,
+      search: search,
+      rowcount: posts.data.total,
+    }
+  },
+
+  methods: {
+    changePage(page) {
+      this.$router.push({
+        path: this.$route.path,
+        query: {
+          q: this.$route.query.q,
+          page: page,
+        },
+      })
+    },
+    //searchData
+    searchData() {
+      this.$router.push({
+        path: this.$route.path,
+        query: {
+          q: this.search,
+        },
+      })
+    },
+
+    //deletePost method
+    deleteRole(id) {
+      this.$swal
+        .fire({
+          title: 'APAKAH ANDA YAKIN ?',
+          text: 'INGIN MENGHAPUS DATA INI !',
+          icon: 'warning',
+          showCancelButton: true,
+          confirmButtonColor: '#d33',
+          cancelButtonColor: '#3085d6',
+          confirmButtonText: 'YA, HAPUS!',
+          cancelButtonText: 'TIDAK',
+        })
+        .then((result) => {
+          if (result.isConfirmed) {
+            //delete tag from server
+
+            this.$axios.delete(`/api/admin/workflow/process/${id}`).then((response) => {
+              //feresh data
+              this.$nuxt.refresh()
+              if (response.data.success == true) {
+                this.sweet_alert.title = 'BERHASIL!'
+                this.sweet_alert.icon = 'success'
+              } else {
+                this.sweet_alert.title = 'GAGAL!'
+                this.sweet_alert.icon = 'error'
+              }
+
+              //alert
+              this.$swal.fire({
+                title: this.sweet_alert.title,
+                text: response.data.message,
+                icon: this.sweet_alert.icon,
+                showConfirmButton: false,
+                timer: 2000,
+              })
+            })
+          }
+        })
+    },
+
+    exportData() {
+      const headers = {
+        'Content-Type': 'application/json',
+      }
+
+      this.$axios({
+        url: `/api/admin/workflow/process/export`,
+        method: 'GET',
+        responseType: 'blob',
+        headers: headers, // important
+      }).then((response) => {
+        this.isLoading = false
+        const url = window.URL.createObjectURL(new Blob([response.data]))
+        const link = document.createElement('a')
+        link.href = url
+        var fileName = 'WorkflowProcess.xlsx'
+        link.setAttribute('download', fileName) //or any other extension
+        document.body.appendChild(link)
+        link.click()
+      })
+    },
+  },
+}
+</script>
+
+<style scoped></style>
