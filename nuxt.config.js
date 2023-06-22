154010ad779be47cc3a10e3b86e936d64071150a
rendrasuwondo <rendrasuwondo@gmail.com>
2023-06-22 15:54:48 +0700

    Configj

--- nuxt.config.js
+++ nuxt.config.js
@@ -125,7 +125,7 @@ export default {
 
     // Axios module configuration: https://go.nuxtjs.dev/config-axios
     axios: {
-        baseURL: 'http://192.168.30.23:8003',
+        baseURL: 'http://192.168.30.22:8003',
     },
 
     // Build Configuration: https://go.nuxtjs.dev/config-build
